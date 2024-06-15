9cf2570abaac4dfcd9875bf4a8cc5c385f0ae5b3
vyacheslav <skavik46111@gmail.com>
2024-06-15 14:40:39 +0300

    added some formating

--- handlers/converters/chats.py
+++ handlers/converters/chats.py
@@ -2,10 +2,10 @@ from dtos.mesages import ChatListItemDTO
 
 
 def convert_chats_dtos_to_message(chats: list[ChatListItemDTO]) -> str:
-    return '\n'.join(
+    return '\n\n'.join(
         (
             'Список всех доступных чатов:',
-            '\n'.join(
+            '\n\n'.join(
                 (f'ChatOID: `{chat.oid}`\. Проблема: {chat.title}' for chat in chats)  # type: ignore
             )
         )
